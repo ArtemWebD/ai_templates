e4afa178dcc8d4bed801026a4922449ce452b7b3
artemWebD <artemtoptun@gmail.com>
2025-01-13 17:00:47 +0700

    created router for white page

--- modules/routes/white-page-router.js
+++ modules/routes/white-page-router.js
@@ -1,12 +1,26 @@
 import { Router } from "express";
 import multer from "multer";
 import authMiddleware from "../../middlewares/auth.middleware.js";
+import superAuthMiddleware from "../../middlewares/superAuth.middleware.js";
+import whitePageController from "../white-page/whitePage.controller.js";
 
 export const whitePageRouter = new Router();
 const upload = multer();
 
 //Upload template
-whitePageRouter.post("/white-page/upload", upload.single("site"), authMiddleware, whitePageController.uploadTemplate);
+whitePageRouter.post("/white-page/upload", upload.single("site"), authMiddleware, superAuthMiddleware, whitePageController.uploadTemplate);
+
+//Get all templates
+whitePageRouter.get("/white-page/", authMiddleware, whitePageController.getAll);
+
+//Remove template
+whitePageRouter.delete("/white-page/:id", authMiddleware, superAuthMiddleware, whitePageController.remove);
 
 //Generate site by template
 whitePageRouter.post("/white-page/create", authMiddleware, whitePageController.create);
+
+//Get json file of template
+whitePageRouter.get("/white-page/json/:id", authMiddleware, superAuthMiddleware, whitePageController.getJson);
+
+//Edit json file of template
+whitePageRouter.put("/white-page/json", authMiddleware, superAuthMiddleware, whitePageController.updateJson);
