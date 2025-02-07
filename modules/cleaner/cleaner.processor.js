39e88523162e552834f554a2a15c0dbb85ca77fd
artemWebD <artemtoptun@gmail.com>
2025-02-07 15:17:44 +0700

    cleaner class

---
+++ modules/cleaner/cleaner.processor.js
@@ -0,0 +1,54 @@
+import path from "path";
+import fs from "fs/promises";
+import generatedWhitePageService from "../generatedWhitePage/generatedWhitePage.service.js";
+
+class CleanerProcessor {
+    async clean(job, done) {
+        try {
+            const lifetime = job.data.lifetime;
+            const dirPath = path.resolve() + job.data.path;
+
+            const files = await fs.readdir(dirPath);
+
+            if (!files.length) {
+                done();
+                return;
+            }
+
+            for (const fileName of files) {
+                const filePath = path.join(dirPath, fileName);
+                const stat = await fs.stat(filePath);
+
+                //File creation time in ms
+                const now = new Date();
+                const fileCreationTime = now - stat.birthtime;
+
+                if (fileCreationTime >= lifetime) {
+                    await fs.rm(filePath, { recursive: true });
+                }
+            }
+
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
+    async cleanDatabase(job, done) {
+        try {
+            const lifetime = job.data.lifetime;
+            const models = await generatedWhitePageService.getByTime(lifetime);
+
+            for (const model of models) {
+                await generatedWhitePageService.remove(model.id);
+            }
+
+            done();
+        } catch (error) {
+            console.log(error);
+            done(error);
+        }
+    }
+}
+
+export default new CleanerProcessor();
